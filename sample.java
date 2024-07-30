3fb0edd65b42ad66470b17215f93f2b0b32773f0
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-30 19:40:38 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+public class sample{
+  sample() {
+      Frame frame =new Frame("ARUN");
+      Button but = new Button("Click Me");
+
+     frame.add(but);
+
+     frame.setLayout(new FlowLayout());
+      frame.setVisible(true);
+      frame.setSize(400, 300);
+  }
+  public static void main(String[] args) {
+      new sample();
+  }
+
+}
