722d34c324ecd4ec0e8be35047194fcbcd057426
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-31 19:31:10 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,9 +3,7 @@ public class sample{
   sample() {
       Frame frame =new Frame("ARUNKUMAR");
       Button but = new Button("Click Me");
-
      frame.add(but);
-
      frame.setLayout(new FlowLayout());
       frame.setVisible(true);
       frame.setSize(400, 300);
